8953b63119f1fd5123a45392128ac14081b93cda
caezar47 <iss47@ya.ru>
2017-06-26 09:11:42 +0300

    ready

---
+++ src/block/site/default/head/.document-ready.js
@@ -0,0 +1,6 @@
+$('img').addClass('img-responsive');
+$('.text-block ul').addClass('ul-site');
+
+$('.text-block table').addClass('table table-bordered');
+$('.text-block .table.table-bordered').wrap('<div class="table-responsive"></div>'); 
+$('.text-block img').parent().addClass('_tb__img'); 
